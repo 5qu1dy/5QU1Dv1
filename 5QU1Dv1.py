4b6ea5f6c5cddc6f70dcea67aca3869dfb5d834d
Oktavianuzzz <48722600+Oktavianuzzz@users.noreply.github.com>
2019-04-01 17:30:42 +0700

    Add files via upload

---
+++ 5QU1Dv1.py
@@ -0,0 +1,66 @@
+#Saya Harap Tools ini Digunakan dengan BIJAK...!!!
+#Diketik oleh Mr.5qu1dy
+#Python
+
+import os
+import time
+
+os.system ('pkg install toilet;pkg install figlet;pkg install ruby;gem install lolcat;pkg install cowsay;pkg install git;pkg install python')
+time.sleep(1)
+os.system ('clear')
+time.sleep(1)
+os.system ('toilet -f standard "5QUIDv1" -F gay')
+time.sleep(1)
+pembuka='''
+     % Author : Mr.5qu1dy %
+     % Team : Anonymous King Cobra %
+'''
+print (pembuka)
+time.sleep(1)
+user=input ('Username : ')
+if user=='5qu1d':
+   print ('CORRECT')
+   time.sleep(1)
+ktsndi= input ('Password : ')
+if ktsndi=='gw gatau':
+     print ('CORRECT')
+     time.sleep(1)
+     print ('LOGIN')
+     time.sleep(5)
+     print ('LOGIN SUCCESSFULLY')
+     time.sleep(2)
+     os.system ('clear')
+     toilet -f standard "KrizInstaller" -F gay
+     time.sleep(1)
+     print ('	Tools Installer by 5qu1d & Kriz	')
+     time.sleep(1)
+     menu='''
+     <1> ProgramNewbie
+     <2> KrizInstallerV1.0
+     <3> KrizInstallerV1.6
+     
+     <0> Close
+     '''
+     print (menu)
+     pilihan=input ('enter >> ')
+     if pilihan=='1'or pilihan=='01':
+        os.system ("clear")
+        os.system ('apt install python;apt install git;git clone https://github.com/Oktavianuzzz/ProgramNewbie')
+        print ('Silahkan ketik "ls" dan pergi ke cd ProgramNewbie...!')
+        os.system ("clear")
+ 	elif pilihan=='2' or pilihan=='02':
+ 	     os.system ("clear")
+ 	     os.system ("apt install python;apt install git;git clone https://github.com/Oktavianuzzz/KrizInstallerV1.0")
+ 	     print ("Silahkan ketik 'ls' dan pergi ke cd KrizInstallerV1.0...!")
+ 	     os.system ('clear')
+ 	elif pilihan=='03' or pilihan=='3':
+ 	     os.system ('clear')
+ 	     os.system ("apt install git;apt install python;git clone https://github.com/Oktavianuzzz/KrizInstallerV1.6")
+ 	     print ('Silahkan Ketik "ls" dan pergi ke cd KrizInstallerV1.6...!')
+ 	     os.system ('clear')
+ 	elif pilihan=='0' or pilihan=='00':
+ 	     print ('Thanks To Use my Tools')
+ 	     os.system ("exit")
+ 	
+else:
+   print ('WRONG...!!!')
